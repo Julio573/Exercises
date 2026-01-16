6dd9a75dfd8c8029cc0b5da051def4f502e5055a
julio573 <julio8497@hotmail.com>
2026-01-16 14:29:04 -0300

    Product exercise class

---
+++ NelioCourse-Exercises/src/ex13/Product.java
@@ -0,0 +1,36 @@
+package ex13;
+
+public class Product {
+
+    private String productName;
+    private double productPrice;
+
+    public Product() {
+    }
+
+    public Product(String productName, double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return getProductName() + " $" + String.format("%.2f", productPrice);
+    }
+}
