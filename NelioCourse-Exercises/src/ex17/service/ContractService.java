80f122098093f288a6a9c05a53945b88f168b756
julio573 <julio8497@hotmail.com>
2026-01-19 11:59:26 -0300

    Contract exercise ContractService Class

---
+++ NelioCourse-Exercises/src/ex17/service/ContractService.java
@@ -0,0 +1,26 @@
+package ex17.service;
+
+import ex17.entities.Contract;
+import ex17.entities.Installment;
+
+import java.time.LocalDate;
+
+public class ContractService {
+
+    private final OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+        double basicQuota = contract.getTotalValue();
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, (int) quota));
+        }
+    }
+}
